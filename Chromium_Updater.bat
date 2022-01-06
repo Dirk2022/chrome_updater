be144f69cfe395bf082a52451640393e2d76658a
Dirk Paehl <97250251+Dirk2022@users.noreply.github.com>
2022-01-06 19:32:22 +0100

    Add files via upload

---
+++ Chromium_Updater.bat
@@ -0,0 +1,4 @@
+powershell ./Chromium_Updater.ps1
+
+start h:\Progs\Chrome_portable\chrome-win\chrome.exe --flag-switches-begin --user-data-dir=..\profile --no-default-browser-check  --disable-logging --disable-breakpad  --autoplay-policy=user-required --flag-switches-end
+exit
